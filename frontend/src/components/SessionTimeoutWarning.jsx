ce3e915d14fad8113b853c4b7343fbc278ab3bb0
takeda-storks <storks.m.takeda@gmail.com>
2025-11-12 14:35:25 +0900

    セッションタイムアウトの修正

--- frontend/src/components/SessionTimeoutWarning.jsx
+++ frontend/src/components/SessionTimeoutWarning.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import '../styles/SessionTimeoutWarning.css';
 
 export default function SessionTimeoutWarning({ timeLeft, onExtend, onLogout }) {
+  const handleLogoutClick = () => {
+    console.log('ログアウトボタンがクリックされました');
+    console.log('onLogout:', onLogout);
+    if (onLogout) {
+      onLogout();
+    } else {
+      console.error('onLogout関数が渡されていません');
+    }
+  };
+
+  const handleExtendClick = () => {
+    console.log('続行ボタンがクリックされました');
+    if (onExtend) {
+      onExtend();
+    }
+  };
+
   return (
     <div className="timeout-modal-overlay">
       <div className="timeout-modal">
@@ -14,10 +31,10 @@ export default function SessionTimeoutWarning({ timeLeft, onExtend, onLogout }) 
           セッションを延長するには「続行」をクリックしてください。
         </p>
         <div className="timeout-actions">
-          <button className="btn-extend" onClick={onExtend}>
+          <button className="btn-extend" onClick={handleExtendClick}>
             続行
           </button>
-          <button className="btn-logout-now" onClick={onLogout}>
+          <button className="btn-logout-now" onClick={handleLogoutClick}>
             ログアウト
           </button>
         </div>
