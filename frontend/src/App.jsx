3673e79da77373487e532e39e526548d94686b76
rikutera2025 <recruit.support.sys@rikutera.co.jp>
2025-11-27 21:10:55 +0900

    AdminRoute関係の修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -127,7 +127,7 @@ function App() {
           <Route path="/my-page" element={<ProtectedRoute element={<MyPages />} isAuthenticated={isAuthenticated} />} />
           <Route path="/howto" element={<ProtectedRoute element={<HowToPage />} isAuthenticated={isAuthenticated} />} />
           <Route path="/teams" element={<AdminRoute element={<TeamsPage />} />} />
-          <Route path="/users" element={<AdminRoute element={<UserManagementPage />} />} />
+          <Route path="/users" element={<AdminOrManagerRoute element={<UserManagementPage />} />} />
           <Route path="/admin/usage" element={<AdminRoute element={<AdminUsageDashboard />} />} />
           <Route path="/login" element={<LoginPage onLoginSuccess={handleLoginSuccess} />} />
         </Routes>
